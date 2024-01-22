5062d74df3730f0cb79d3cb644747d2141043f7d
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-22 15:59:06 +0530

    feat: mogo connection function

--- src/db/MongoConnect.js
+++ src/db/MongoConnect.js
@@ -3,10 +3,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectMDB = async () => {
   try {
-    const connection = await mongoose.connect(
+    await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority`
     );
-    console.log("Database is connected to:", connection.connections[0].name);
   } catch (error) {
     console.log(error);
     process.exit(1); // Stop the app if there is an error
