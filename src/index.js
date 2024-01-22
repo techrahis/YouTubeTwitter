11c3ecd4307a64bcf232c03249ff99802a137bf3
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-01-22 15:59:35 +0530

    feat: mongo and express merges into one promise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectMDB from "./db/MongoConnect.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectMDB();
+connectMDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5600, () => {
+      console.log(
+        `Mongo Connected and Server is listening on port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
